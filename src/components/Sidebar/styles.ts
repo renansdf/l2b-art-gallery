5c477323a2a99db095f9c7bed6eb3d20512c3969
Renan de Freitas <rnn.frts@gmail.com>
2021-04-15 20:00:33 -0300

    created main content interface for aplication

---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,69 @@
+import styled, {css} from 'styled-components';
+
+interface ContainerProps {
+    isVisible: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+    height: 100%;
+    width: 0px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    right: 0;
+    background: var(--l2b-white-full) 0% 0% no-repeat padding-box;
+    overflow-x: hidden;
+    padding-top: 60px;
+    transition: 0.5s;
+
+    ${props => props.isVisible && css`
+        width: 562px;
+    `}
+
+    a{
+        padding: 8px 8px 8px 0px;
+        text-decoration: none;
+        color: var(--l2b-grey);
+        display: block;
+
+        &:hover{
+            color: var(--l2b-dark-red);
+        }
+    }
+
+    &::-webkit-scrollbar-track {
+        background-color: var(--l2b-white);
+    }
+
+    &::-webkit-scrollbar {
+        width: 6px;
+        background: var(--l2b-white);
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: var(--l2b-light-grey);
+    }
+`;
+
+export const Content = styled.div`
+    padding: 30px 70px 70px 70px;
+    bottom: 10px;
+    overflow: hidden;
+`;
+
+export const CloseContentButton = styled.span`
+    font-family: "Volkart_Light";
+    color: var(--l2b-light-grey);
+    float: right;
+    font-size: 40px;
+    position: absolute;
+    top: 30px;
+    right: 23px;
+    z-index: 2;
+
+    &:hover, &:focus{
+        color: var(--l2b-black);
+        text-decoration: none;
+        cursor: pointer;
+    }
+`;
