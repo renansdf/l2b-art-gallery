6fc7fb2b44b178e19ff0778099ceb26ad0f91eba
Renan de Freitas <rnn.frts@gmail.com>
2021-05-07 15:56:10 -0300

    refactored sidebar styles

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { useSidebar } from '../../hooks/Sidebar';
 
-import GalleryMap from '../GalleryMap';
-import BoasVindas01 from './contents/Boasvindas_hotspot_01';
-
 import {Container, Content, CloseContentButton} from './styles';
 
-interface IContentsData{
-    [name: string]: JSX.Element;
-}
-
 const Sidebar: React.FC = () => {
-    const {isSidebarVisible, setIsSidebarVisible, contentName} = useSidebar();
-
-    const contents: IContentsData = {
-        boasVindas_01: <BoasVindas01 />,
-        galleryMap: <GalleryMap />
-    }
+    const {sidebarVisibility, setSidebarVisibility, content} = useSidebar();
 
     return (
-        <Container isVisible={isSidebarVisible}>
-            <CloseContentButton onClick={() => setIsSidebarVisible(!isSidebarVisible)}>&times;</CloseContentButton>
+        <Container isVisible={sidebarVisibility}>
+            <CloseContentButton onClick={() => setSidebarVisibility(!sidebarVisibility)}>&times;</CloseContentButton>
             <Content>
-                {contents[contentName]}
+                {content}
             </Content>
         </Container>
     )
