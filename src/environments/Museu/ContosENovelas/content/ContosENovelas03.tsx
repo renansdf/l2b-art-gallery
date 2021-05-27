3f0882e9231c9b0a73698392c8084c4da2987bae
Renan de Freitas <rnn.frts@gmail.com>
2021-05-27 00:07:36 -0300

    refactored into other optized files

--- src/environments/Museu/ContosENovelas/content/ContosENovelas03.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ContosENovelas03: React.FC = () => {
-  return(
-    <h1>Contos E Novelas 3</h1>
-  );
-}
-
-export default ContosENovelas03
--- src/environments/Museu/ContosENovelas/content/ContosENovelas04.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ContosENovelas04: React.FC = () => {
-  return(
-    <h1>Contos E Novelas 4</h1>
-  );
-}
-
-export default ContosENovelas04
