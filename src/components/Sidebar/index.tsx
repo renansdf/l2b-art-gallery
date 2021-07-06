df2b772c6750cf7551b121ebbe058cbd310c5995
Renan de Freitas <rnn.frts@gmail.com>
2021-07-06 00:00:19 -0300

    added function to return camera to navigation on content close

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useClosedCamera } from '../../hooks/ClosedCamera';
 import { useSidebar } from '../../hooks/Sidebar';
 
 import {Container, Content, CloseContentButton} from './styles';
 
 const Sidebar: React.FC = () => {
     const {sidebarVisibility, setSidebarVisibility, content} = useSidebar();
+    const {cameraReturnNavigation} = useClosedCamera();
+
+    const handleClose = () => {
+        setSidebarVisibility(!sidebarVisibility)
+        cameraReturnNavigation();
+    }
 
     return (
         <Container isVisible={sidebarVisibility}>
-            <CloseContentButton onClick={() => setSidebarVisibility(!sidebarVisibility)}>&times;</CloseContentButton>
+            <CloseContentButton onClick={handleClose}>&times;</CloseContentButton>
             <Content>
                 {content}
             </Content>
