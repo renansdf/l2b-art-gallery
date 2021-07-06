cf4c4c793c9e1abbcff32435574b34c8fe9ef450
Renan de Freitas <rnn.frts@gmail.com>
2021-07-05 23:59:35 -0300

    created default orientations for camera

---
+++ src/helpers/orientations.ts
@@ -0,0 +1,8 @@
+const orientations = {
+  direita: {x:0, y:1.576, z:0},
+  esquerda: {x:0, y:-1.576, z:0},
+  frente: {x:0, y:0, z:0},
+  costas: {x:0, y:3.135, z:0}
+}
+
+export default orientations
