578122792dabe1a84b5f4a8ae060fbdcad4c2ad9
Renan de Freitas <rnn.frts@gmail.com>
2021-07-23 18:56:39 -0300

    shows over the loading screen of the galery

---
+++ src/components/Instructions/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Container, Content, Slide, CloseInstructions } from './styles';
+
+const Instructions: React.FC = () => {
+  const [visibility, setVisibility] = useState(true);
+
+  return (
+    <Container isVisible={visibility}>
+      <Content>
+        <Slide>
+          <h1>Bem vindo a galeria!</h1>
+          <p>Para olhar ao redor, segure o botão de clique e arraste em qualquer direção</p>
+        </Slide>
+        <Slide>
+          <p>Para andar pela galeria, clique uma vez no chão a sua frente. Atenção! Você só conseguirá clicar em lugares perto de você.</p>
+        </Slide>
+        <Slide>
+          <p>Perto de cada obra haverá um icone de olho. Clique no ícone para visualizar a obra de perto e saber mais detalhes.</p>
+        </Slide>
+        
+        <CloseInstructions onClick={() => setVisibility(!visibility)} />
+      </Content>
+    </Container>
+  );
+}
+
+export default Instructions;
