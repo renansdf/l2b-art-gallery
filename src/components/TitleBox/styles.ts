090c2a8b374aa04ca269d3899530fd5378104c74
Renan de Freitas <rnn.frts@gmail.com>
2021-08-27 15:47:23 -0300

    detect mouse position over hotspots to determine title to show

---
+++ src/components/TitleBox/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff;
+  border-radius: 20px;
+  font-size: 40px;
+  font-weight: bold;
+  color: #777;
+  padding: 20px 35px;
+  box-shadow: 0px 0px 20px -7px #888;
+
+  position: fixed;
+  left: 50%;
+  top: 40px;
+  transform: translateX(-50%);
+  z-index: 90;
+`
