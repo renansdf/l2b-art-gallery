87f9d1eb99bae036114b80313cd191d74adf1324
renansdf <rnn.frts@gmail.com>
2021-11-05 20:08:00 -0300

    added keys to fix warning about repeated elements

--- src/components/Instructions/index.tsx
+++ src/components/Instructions/index.tsx
@@ -19,16 +19,16 @@ const Instructions: React.FC = () => {
   }
 
   const content = [
-    <Slide>
+    <Slide key="slide1">
       <h1>bem-vindo!</h1>
       <p>A L2B é um espaço virtual dedicado às artes</p>
       <p>além de exibir o acervo próprio convida outros artistas para apresentar suas criações em nossos espaços</p>
     </Slide>,
-    <Slide>
+    <Slide key="slide2">
       <img src={mouse} alt="mouse" />
       <p>Clique no chão para caminhar<br/>Clique e deslize para girar</p>
     </Slide>,
-    <Slide>
+    <Slide key="slide3">
       <img src={hotspot} alt="mouse" />
       <p>Perto de cada obra haverá um icone de olho.<br />Clique no ícone para visualizar a obra de perto e saber mais detalhes.</p>
     </Slide>
