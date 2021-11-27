71590cd5c2ddd1a2ea4b81b7e7027c494f973421
renansdf <rnn.frts@gmail.com>
2021-11-27 15:49:55 -0300

    now only has top shadow

--- src/components/GalleryHud/index.tsx
+++ src/components/GalleryHud/index.tsx
@@ -1,47 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useClosedCamera } from '../../hooks/ClosedCamera';
-
-import mapBemVindos from '../../assets/img/mapBemVindos.png';
-import mapContosENovelas from '../../assets/img/mapContosENovelas.png';
-import mapEnsaios from '../../assets/img/mapEnsaios.png';
-import mapExposicoes from '../../assets/img/mapExposicoes.png';
-import mapInfantil from '../../assets/img/mapInfantil.png';
-import mapPoesias from '../../assets/img/mapPoesias.png';
-import mapObservatorio from '../../assets/img/mapObservatorio.png';
-
-import {Container, Shadow} from './styles'
+import React from 'react';
+import { Shadow } from './styles'
 
 const GalleryHud: React.FC = () => {
-  const [location, setLocation] = useState<string>('imgsrc');
-  const {cameraCoordinates} = useClosedCamera();
-
-  useEffect(() => {
-    if(cameraCoordinates.x > 5.4){
-      if(cameraCoordinates.y > 2){
-        setLocation(mapObservatorio);
-      } else {
-        setLocation(mapExposicoes);
-      }
-    } else if(cameraCoordinates.x < 5.4 && cameraCoordinates.x > -5.4){
-      setLocation(mapBemVindos);
-    } else {
-      if(cameraCoordinates.y > 2 && cameraCoordinates.y < 5){
-        setLocation(mapContosENovelas);
-      } else if(cameraCoordinates.y > 5){
-        setLocation(mapPoesias);
-      } else if(cameraCoordinates.x < -22){
-        setLocation(mapEnsaios);
-      } else {
-        setLocation(mapInfantil);
-      }
-    }
-  }, [cameraCoordinates]);
 
   return (
-    <Container>
-      <img src={location} alt={location} />
-      <Shadow />
-    </Container>
+    <Shadow />
   )
 }
 
--- src/components/GalleryHud/styles.ts
+++ src/components/GalleryHud/styles.ts
@@ -1,26 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  position: fixed;
-  bottom: 50px;
-  left: 30px;
-  z-index: 11;
-  
-  img{
-    max-width: 300px;
-    max-height: 150px;
-    z-index: 2;
-    position: relative;
-  }
-`;
-
 export const Shadow = styled.span`
   z-index: 1;
   position: fixed;
-  bottom: 0;
+  top: 0;
   left: 0;
   right: 0;
   height: 200px;
-  background: linear-gradient(rgba(32,32,32,0), rgba(32,32,32,.45), rgba(32,32,32,.7));
+  background: linear-gradient(rgba(32,32,32,.7), rgba(32,32,32,.45), rgba(32,32,32,0));
   pointer-events: none;
 `;
