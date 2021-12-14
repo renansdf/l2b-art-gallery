e45c15b5cbd0d7a0998167c106c7d8931d748cfb
renansdf <rnn.frts@gmail.com>
2021-12-14 14:59:34 -0300

    update gizmo src img

--- src/components/Camera/NextPositionGizmo/index.tsx
+++ src/components/Camera/NextPositionGizmo/index.tsx
@@ -20,7 +20,7 @@ const NextPositionGizmo: React.FC<IGizmoProps> = ({position}) => {
     }, [position]);
 
     return (
-        <a-entity ref={gizmo} id="nextPositionGizmo" position={position} scale="1 1 1" rotation="90 0 0" material="side: double; color : #ff0000; transparent: true; opacity: 0;" src={groundmark} geometry="primitive : circle; radius : 0.25" animation__fadein="property : components.material.material.opacity; from : 0; to : 1; dur :500; startEvents : fadein;" animation__fadeout="property : components.material.material.opacity; from : 1; to : 0; dur : 500; startEvents : fadeout;"></a-entity>
+        <a-entity ref={gizmo} id="nextPositionGizmo" position={position} scale="1 1 1" rotation="90 0 0" material={`side: double; color: #ff1111; src: url(${groundmark}); transparent: true; opacity: 0;`} geometry="primitive : circle; radius : 0.25" animation__fadein="property : components.material.material.opacity; from : 0; to : 1; dur :500; startEvents : fadein;" animation__fadeout="property : components.material.material.opacity; from : 1; to : 0; dur : 500; startEvents : fadeout;"></a-entity>
     );
 }
 
