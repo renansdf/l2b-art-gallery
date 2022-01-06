09c741bb53fc228f412837fd29635f0d6fce7892
renansdf <rnn.frts@gmail.com>
2022-01-06 17:10:37 -0300

    updated images and names

--- src/components/AdminGaleryMap/index.tsx
+++ src/components/AdminGaleryMap/index.tsx
@@ -8,18 +8,18 @@ import ContosENovelas from '../../assets/img/4-Contos-e-Novelas.png';
 import Ensaios from '../../assets/img/5-Ensaios.png';
 import Poesias from '../../assets/img/6-Poesias.png';
 import Observatorio from '../../assets/img/7-observatorio.png';
-import ObservatorioPopup from '../../assets/img/mapObservatorio.png';
+import Modal from '../../assets/img/mapModal.png';
 
 const imageList = [
-    {name: 'Modal de Abertura', img: BemVindos, links: ['YagjLBIAACEAWFY3']},
+    {name: 'Modal de Abertura', img: Modal, links: ['YagjLBIAACEAWFY3']},
     {name: 'Bem Vindos', img: BemVindos, links: ['YK7_XBAAACYAYEN3','YK7_dhAAACQAYEP0','YK7_mhAAACQAYESU']}, 
     {name: 'Exposições', img: Exposicoes, links: ['YK8CYxAAACUAYFDH','YK8LaRAAACMAYHkL']}, 
-    {name: 'Infantil', img:Infantil, links: ['YK7jQhAAACMAX8bf','YK75phAAACUAYCos','YK75vRAAACMAYCqR', 'YK751BAAACMAYCsB']}, 
+    {name: 'Infantis', img:Infantil, links: ['YK7jQhAAACMAX8bf','YK75phAAACUAYCos','YK75vRAAACMAYCqR','YK751BAAACMAYCsB']}, 
     {name: 'Contos e Novelas', img: ContosENovelas, links: ['YK7F4xAAACUAX0KH','YK7NYBAAACQAX2On','YK7NexAAACUAX2Qp','YK7NlxAAACMAX2Sz']}, 
     {name: 'Ensaios', img:Ensaios, links: ['YK7cOxAAACMAX6YT','YK7cTBAAACQAX6Zi','YK7cYBAAACUAX6bF','YK7ccRAAACMAX6cU','YK7cgxAAACQAX6do','YK7ckhAAACYAX6ez']}, 
     {name: 'Poesias', img: Poesias, links: ['YKgtiBEAACIAc1rT','YK6ssxAAACYAXtDp','YK66nRAAACMAXw_m','YK66xBAAACYAXxCg','YK663hAAACMAXxEX','YK66-BAAACMAXxGN','YK67JhAAACMAXxJh','YK67DhAAACQAXxHz']},
     {name: 'Observatório', img: Observatorio, links: ['YWoVlhEAACQAky7U','YWoV4REAACUAkzAo','YWoV_hEAACMAkzCs','YWoWIxEAACQAkzFQ','YWoWQhEAACYAkzHi','YWoWXREAACYAkzJa', 'YYWmZxAAACMAhbCx']},
-    {name: 'Popup do Observatório', img: ObservatorioPopup, links: ['YYRTkhAAACIAf_Sd']}
+    {name: 'Modal do Observatório', img: Modal, links: ['YYRTkhAAACIAf_Sd']}
 ]
 
 const AdminGaleryMap: React.FC = () => {
